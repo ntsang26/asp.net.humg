32f3ec02f299883ee5368d578417ff42abc049f9
bogdev <bogdev@bogdevs-MacBook-Air.local>
2022-10-05 23:42:44 +0700

    create new app and new branch

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NguyenThanhSangBTH2.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
